e6205cd6aedcab722777634af9dafbfee3bf5e03
Pranav <pranavsupugade@gmail.com>
2026-05-21 16:55:57 +0530

    updated api url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "baseURL: "baseURL: "YOUR_RAILWAY_BACKEND_URL/api"
 });
 
 export default API;
